da083785ad550cdf5691ab7da3524211addc8c03
Insu <tukhtarovilkhom@gmail.com>
2026-03-07 21:42:29 +0500

    Small fixes in usecase

--- core/usecases/get_leetify_summary.py
+++ core/usecases/get_leetify_summary.py
@@ -13,7 +13,6 @@ class GetLeetifySummary:
         worker = LeetifyAPIDataWorker(data)
         summary = worker.get_matches_stats_summary()
         player = worker.get_player_info()
-        s = worker.get_top_matches()
 
         if not summary:
             return "No data available."
@@ -23,5 +22,4 @@ class GetLeetifySummary:
             f"Matches: {summary['total_matches']}\n"
             f"Avg KD: {summary['avg_kd']}\n"
             f"Avg Rating: {summary['avg_rating']}"
-            f"AVG Stats {s}"
         )
